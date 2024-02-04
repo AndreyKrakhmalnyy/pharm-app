fe9a956f80840c47b9034d96095563a4db2c887e
Andrey <kas190697@gmail.com>
2024-02-05 00:36:33 +0300

    Добавление маршрутов для JWT и для hello из authorization.views.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from authorization import views
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from products.views import CategoryApiView, SubCategoryApiView, ProductApiView, InstructionsApiView
@@ -16,16 +17,18 @@ router.register(r'api/products', ProductApiView, basename='products')
 router.register(r'api/instructions', InstructionsApiView, basename='instructions')
 
 urlpatterns = [
+    path('hello/', views.HelloView.as_view(), name='hello'),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     #______________API_______________#
-    path('api/', include(router.urls)), # Подставляем автоматически созданные URL-маршруты от router выше 
+    path('', include(router.urls)), # Подставляем автоматически созданные URL-маршруты от router выше 
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     #______________Swagger_______________#
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    #______________JWT_______________#
+    path('JWT/api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('JWT/api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('JWT/api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
