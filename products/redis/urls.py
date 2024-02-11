b1d3d73da119d925a381e32b7453e36d5e0e8a8f
Andrey <kas190697@gmail.com>
2024-02-12 01:43:22 +0300

    Удаление папки redis

--- products/redis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from ..views import manage
-
-urlpatterns = {
-    path('<slug:key>', manage, name='manage')
-}
-
-urlpatterns = format_suffix_patterns(urlpatterns)
