822cd1c9df5ace10fa402c617c0bbbb4a35f53b2
Andrey <kas190697@gmail.com>
2024-02-11 23:42:07 +0300

    Добавление debug toolbar

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,21 @@
+# DJANGO settings
 from django.contrib import admin
 from django.urls import path, include
+# authorization/views.py
 from authorization import views
+# DJANGO REST FRAMEWORK
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+# products/views.py
 from products.views import CategoryApiView, SubCategoryApiView, ProductApiView, InstructionsApiView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
     SpectacularRedocView
 )
+# DEBUG TOOLBAR settings
+import debug_toolbar
+
 
 router = routers.DefaultRouter() # Используется для упрощения процесса создания URL-маршрутов для представлений API
 router.register(r'api/category', CategoryApiView, basename='category')
@@ -20,17 +27,19 @@ urlpatterns = [
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
-    # _____________API______________ #
+    # API
     path('', include(router.urls)), # Подставляем автоматически созданные URL-маршруты от router выше 
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # _____________SWAGGER______________ #
+    # SWAGGER
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    # _____________JWT______________ #
+    # JWT
     path('JWT/api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('JWT/api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('JWT/api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    # _____________REDIS______________ #
-    path('products/', include('products.urls')),
+    # REDIS
+    path('products/', include('products.redis.urls')),
+    # DEBUG TOOLBAR
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
